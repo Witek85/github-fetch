cfc188a11f522f53e8bf87570f380468ad6d8a4f
Witek85 <witold.szklarski@gmail.com>
2017-11-22 23:57:15 +0100

    list issues ngfor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 	issues = [
-		{date: '12.01.2015', issue: 'Lorem ipsum dolor sit amet', status: 'open'},
-		{date: '12.01.2015', issue: 'Maecenas pellentesque', status: 'open'},
-		{date: '12.03.2015', issue: 'Etiam nunc fringilla aliquet. Quisque', status: 'open'},
-		{date: '12.05.2015', issue: 'Fusce iaculis, purus fringilla', status: 'open'},
-		{date: '12.06.2015', issue: 'Morbi eleifend. Sed eget', status: 'open'}
+		{date: '12.01.2015', text: 'Lorem ipsum dolor sit amet', status: 'open'},
+		{date: '12.01.2015', text: 'Maecenas pellentesque', status: 'open'},
+		{date: '12.03.2015', text: 'Etiam nunc fringilla aliquet. Quisque', status: 'open'},
+		{date: '12.05.2015', text: 'Fusce iaculis, purus fringilla', status: 'open'},
+		{date: '12.06.2015', text: 'Morbi eleifend. Sed eget', status: 'open'}
 	];
 
   constructor() { }
