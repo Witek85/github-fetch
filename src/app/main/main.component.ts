bf2171492bc0c2269c2b1e161f0a147a469fe054
Witek85 <witold.szklarski@gmail.com>
2017-11-23 00:26:31 +0100

    list group ngfor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,13 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-	issues = [
-		{date: '12.01.2015', text: 'Lorem ipsum dolor sit amet', status: 'open'},
-		{date: '12.01.2015', text: 'Maecenas pellentesque', status: 'open'},
-		{date: '12.03.2015', text: 'Etiam nunc fringilla aliquet. Quisque', status: 'open'},
-		{date: '12.05.2015', text: 'Fusce iaculis, purus fringilla', status: 'open'},
-		{date: '12.06.2015', text: 'Morbi eleifend. Sed eget', status: 'open'}
-	];
+
+	issues: any[] = [
+    {
+      'date': '12.01.2015',
+      'text': [
+        {
+          'name': 'Lorem ipsum dolor sit amet',
+          'status': 'open'
+        },
+        {
+          'name': 'Maecenas pellentesque',
+          'status': 'closed'
+        },
+      ]
+    },
+    {
+      'date': '12.03.2015',
+      'text': [
+        {
+          'name': 'Etiam nunc fringilla aliquet',
+          'status': 'open'
+        }
+      ]
+    },
+    {
+      'date': '12.05.2015',
+      'text': [
+        {
+          'name': 'Fusce iaculis, purus fringilla',
+          'status': 'open'
+        }
+      ]
+    },
+    {
+      'date': '12.06.2015',
+      'text': [
+        {
+          'name': 'Morbi eleifend. Sed eget',
+          'status': 'open'
+        }
+      ]
+    }
+  ];
 
   constructor() { }
 
