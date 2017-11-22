574ffe3f5cac3962c237760c578ccbda5d80884f
Witek85 <witold.szklarski@gmail.com>
2017-11-22 23:47:05 +0100

    bootstrap

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+	issues = [
+		{date: '12.01.2015', issue: 'Lorem ipsum dolor sit amet', status: 'open'},
+		{date: '12.01.2015', issue: 'Maecenas pellentesque', status: 'open'},
+		{date: '12.03.2015', issue: 'Etiam nunc fringilla aliquet. Quisque', status: 'open'},
+		{date: '12.05.2015', issue: 'Fusce iaculis, purus fringilla', status: 'open'},
+		{date: '12.06.2015', issue: 'Morbi eleifend. Sed eget', status: 'open'}
+	];
 
   constructor() { }
 
