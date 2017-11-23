e0461b4f79a4094ddf8c687c2c7dfb82db5bbdc2
Witek85 <witold.szklarski@gmail.com>
2017-11-23 23:28:58 +0100

    property binding filter

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,47 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   chosen = 'all';
-	issues: any[] = [
-    {
-      'date': '12.01.2015',
-      'text': [
-        {
-          'name': 'Lorem ipsum dolor sit amet',
-          'status': 'open'
-        },
-        {
-          'name': 'Maecenas pellentesque',
-          'status': 'closed'
-        },
-      ]
-    },
-    {
-      'date': '12.03.2015',
-      'text': [
-        {
-          'name': 'Etiam nunc fringilla aliquet',
-          'status': 'open'
-        }
-      ]
-    },
-    {
-      'date': '12.05.2015',
-      'text': [
-        {
-          'name': 'Fusce iaculis, purus fringilla',
-          'status': 'open'
-        }
-      ]
-    },
-    {
-      'date': '12.06.2015',
-      'text': [
-        {
-          'name': 'Morbi eleifend. Sed eget',
-          'status': 'open'
-        }
-      ]
-    }
+	issues = [
+    {'date': '12.01.2015', 'name': 'Lorem ipsum dolor sit amet', 'status': 'open' },
+    {'date': '12.01.2015', 'name': 'Maecenas pellentesque', 'status': 'closed' },
+    {'date': '12.03.2015', 'name': 'Etiam nunc fringilla aliquet', 'status': 'closed' },
+    {'date': '12.01.2015', 'name': 'Fusce iaculis, purus fringilla', 'status': 'open' },
+    {'date': '12.01.2015', 'name': 'Morbi eleifend. Sed eget', 'status': 'open' }
   ];
 
   constructor() { }
@@ -56,9 +21,13 @@ export class MainComponent implements OnInit {
   ngOnInit() {
   }
 
+  onChoose(status) {
+    this.chosen = status;
+  }
+
   getIssues() {
     if (this.chosen === 'all') {
-      // console.log(this.issues);
+      console.log(this.issues);
       return this.issues.slice();
     }
 
