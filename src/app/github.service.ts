abff6d468d00d281ad004d1a25f5fa26727ec013
Witek85 <witold.szklarski@gmail.com>
2018-12-27 00:04:05 +0100

    issues in issue

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,16 +5,26 @@ import { Subject } from 'rxjs/Subject';
 export class GithubService {
 
 	issues = [
-		{'date': '12.01.2015', 'name': 'Lorem ipsum dolor sit amet', 'status': 'open' },
-		{'date': '12.01.2015', 'name': 'Maecenas pellentesque', 'status': 'closed' },
-		{'date': '12.03.2015', 'name': 'Etiam nunc fringilla aliquet', 'status': 'closed' },
-		{'date': '12.04.2015', 'name': 'Fusce iaculis, purus fringilla', 'status': 'open' },
-		{'date': '12.05.2015', 'name': 'Morbi eleifend. Sed eget', 'status': 'open' },
-		{'date': '12.06.2015', 'name': 'Suspendisse a nibh', 'status': 'open' },
-		{'date': '12.08.2015', 'name': 'Duis pretium erat volutpat', 'status': 'closed' },
-		{'date': '12.09.2015', 'name': 'Maecenas scelerisque', 'status': 'closed' },
-		{'date': '12.10.2015', 'name': 'Etiam blandit suscipit, velit ac', 'status': 'open' },
-		{'date': '12.12.2015', 'name': 'In tempus facilisis', 'status': 'open' }
+		{'date': '12.01.2015', 'issues': [
+			{'name': 'Lorem ipsum dolor sit amet', 'status': 'open'},
+			{'name': 'Maecenas pellentesque', 'status': 'closed'}
+		] },
+		{'date': '12.03.2015', 'issues': [
+			{'name': 'Etiam nunc fringilla aliquet', 'status': 'closed'},
+			{'name': 'Fusce iaculis, purus fringilla', 'status': 'open'},
+			{'name': 'Morbi eleifend. Sed eget', 'status': 'open'}
+		] },
+		{'date': '12.06.2015', 'issues': [
+			{'name': 'Suspendisse a nibh', 'status': 'open'}
+		] },
+		{'date': '12.08.2015', 'issues': [
+			{'name': 'Duis pretium erat volutpat', 'status': 'closed'},
+			{'name': 'Maecenas scelerisque', 'status': 'closed'}
+		] },
+		{'date': '12.10.2015', 'issues': [
+			{'name': 'Etiam blandit suscipit, velit ac', 'status': 'open'},
+			{'name': 'In tempus facilisis', 'status': 'open'}
+		] }
 	];
 	chosen = 'all';
 	statusChange = new Subject<void>();
@@ -26,18 +36,18 @@ export class GithubService {
 			return this.issues.slice();
 		}
 
-		return this.issues.filter((issue) => {
-			return issue.status === chosen;
-		});
+		// return this.issues.filter((issue) => {
+		// 	return issue.status === chosen;
+		// });
 	}
 
 	onModifyStatus(event) {
-		const pos = this.issues.findIndex((iss) => {
-			return iss.name === event.name;
-		});
+		// const pos = this.issues.findIndex((iss) => {
+		// 	return iss.name === event.name;
+		// });
 
-		this.issues[pos].status = event.status; 
-		this.statusChange.next();
+		// this.issues[pos].status = event.status; 
+		// this.statusChange.next();
 	}
 
 	getChosen() {
